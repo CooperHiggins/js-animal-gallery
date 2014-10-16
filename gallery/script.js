0da3e26b3dc7a198808e70f97cf31fc73bd82032
CooperHiggins <cooperhiggins@gmail.com>
2014-10-15 19:10:20 -0500

    edited doc

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,26 @@
 // Add your code here
+$(".filter-button").on('click', function(){
+	var animal = this.id;
+	$("." + this.id).toggle();
+	$("#" + this.id).toggleClass("active");
+
+    console.log("the value of this is ...", this.id); 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
